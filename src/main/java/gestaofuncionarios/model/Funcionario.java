f781a2ec96b532642a021d86d83d4644a4e45c3a
Matheus Rocha <43797621+matheuzsr@users.noreply.github.com>
2022-11-11 02:16:12 -0300

    Update Funcionario.java

--- src/main/java/gestaofuncionarios/model/Funcionario.java
+++ src/main/java/gestaofuncionarios/model/Funcionario.java
@@ -77,7 +77,6 @@ public class Funcionario implements Comparable<Funcionario> {
     }
 
     public LocalDate getDataAdmissao() {
-        System.out.println(this.dataAdmissao);
         return this.dataAdmissao;
     }
 
